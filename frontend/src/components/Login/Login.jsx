7c86bf39e1888de1729a523ecf3b2bbd7edffa01
John Dale <113763700+dingdingwall@users.noreply.github.com>
2023-11-19 01:37:11 +0800

    Login
    login design kapoy

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+
+import email_icon from '../Assets/email.png'
+import  password_icon from '../Assets/password.png'
+
+import './Login.css'
+
+const Login = () => {
+  return (
+  
+     <div className='container'>
+       <div className="header">
+         <div className="text">Login</div>
+         <div className="underline"></div>
+       </div>
+       <div className="inputs">
+        
+       
+       
+        <div className="input"> 
+        <img src={email_icon} alt =''/>
+         <input type = "email" placeholder='email'/>  </div>
+        
+         <div className="input">  
+       <img src={password_icon} alt =''/>
+         <input type = "password" placeholder='password'/></div>
+
+       <div className="forgot">Forgot Password? <span>Click Here!</span></div>
+
+       <div className="submit_container">
+        <div className="sign_btn"><button>Sign Up</button></div>
+        <div className="login_btn"><button>Login</button></div>
+
+         
+       </div>
+
+
+       
+
+      
+       
+       </div>
+
+     </div>
+    
+  )
+}
+
+export default Login
