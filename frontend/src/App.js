3b5875b6c1994543d35ee15a63943d79ba5cb762
John Dale <113763700+dingdingwall@users.noreply.github.com>
2023-11-24 21:03:47 +0800

    I added a sign up and sign up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,46 @@
 
-import Login from "./components/Login/Login";
-
-
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../src/index.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Login from './components/Login/Login.jsx'
+import SignUp from './components/SignUp/SignUp.jsx'
 function App() {
-  return <div> <Login></Login> 
- </div>;
-};
-
-export default App;
+  return (
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/sign-in'}>
+              Team Tres Lang
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
+}
+export default App
