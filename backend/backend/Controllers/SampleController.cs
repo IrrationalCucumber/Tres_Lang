f76872a5f1bf352b65d9b3b30a158df9168387db
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-11-24 14:30:55 +0800

    added 'entry Table' MVC

---
+++ backend/backend/Controllers/SampleController.cs
@@ -0,0 +1,73 @@
+﻿using backend.Views;
+using backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using backend.Entities;
+
+namespace backend.Controllers
+{
+    [Route("[action]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> Sample()
+        {
+            List<string> list = new List<string> { "first", "second", "third", "fourth", "fifth", "sixth" };
+
+            return list;
+        }
+
+        [HttpGet]
+        public string Duhig(string name)
+        {
+            return "WELCOME " + name;
+        }
+
+
+        [HttpPost]
+        public string user(UserModel model)
+        {
+            using (var context = new dbContext())
+            {
+                context.Users.Add(new user
+                {
+                    Username = model.Username,
+                    Password = model.Password,
+                    Fname = model.Fname,
+                    Lname = model.Lname,
+                    Bday = model.Bday,
+                    Age = model.Age,
+                    Gender = model.Gender,
+                    DateCreated = model.DateCreated,
+                });
+
+                context.SaveChanges();
+            }
+            return "SUCCESS";
+        }
+
+        [HttpPost]
+        public string entry(entryModel model)
+        {
+            using (var context = new dbContext())
+            {
+                context.Entry.Add(new entry
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    Location = model.Location,
+                    Date = model.Date,
+                    Type = model.Type,
+                    Url = model.Url,
+                    DatePosted = model.DatePosted,
+                });
+
+                context.SaveChanges();
+            }
+            return "SUCCESS";
+        }
+
+    }
+}
