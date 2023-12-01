4d456c7e1df4dab96cfd1f5cb9ff67ddd68bf850
DESKTOP-TNU1G00\VILFAM <kerviemille@gmail.com>
2023-12-01 17:29:33 +0800

    profile-page with input fields

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
-
+import React from 'react';
+import "../components/Styles/Profile.css"
 export const Profile = () => {
+  
   return (
-    <div className='background'>Profile</div>
+    
+    <React.Fragment>
+     
+      <div className='background'>
+                <contact id='profile'>
+                  <form class="cf">
+                  <center><h1>EDIT PROFILE</h1></center>
+                      <input type="text" id="input-firstname" placeholder="Firstname"></input>
+                      <input type="text" id="input-lastname" placeholder="Lastname"></input>
+                      <input type="text" id="input-password" placeholder="Password"></input>
+                      <input type="text" id="input-birthday" placeholder="Birthday"></input>
+                      <input type="text" id="input-age" placeholder="Age"></input>
+                      <input type="text" id="input-gender" placeholder="Gender"></input>
+                   
+                      <input type="submit" value="SUBMIT" id="input-submit"></input>
+                  </form>
+                </contact>
+                </div> 
+           
+    </React.Fragment>
+    
   )
 }
+
+export default Profile;
