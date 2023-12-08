261620adb9b99928605e51a2659a0a020fef2073
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-12-08 15:10:26 +0800

    upadate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,34 +4,23 @@ import { About } from "./pages/About";
 import Home from "./pages/Home";
 import { Profile } from "./pages/Profile";
 import Login from "./pages/Login";
-
-
-
-
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <div>
-       <BrowserRouter> 
-       <NavBar/>
+      <BrowserRouter>
+        <NavBar />
 
-       <Routes>
-     
-          <Route path='/' element={<Home/>}/>
-          <Route path='/home' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-        
-          <Route path='/profile' element={<Profile/>}/>
-          <Route path='/login' element={<Login/>}/>
-       
-
-
-
-       </Routes>
-        
-        
-        </BrowserRouter>
-     
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile/:userID" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,47 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import "../NavBar/NavBar.css"
-import { Link } from 'react-router-dom'
+import "../NavBar/NavBar.css";
+import { Link } from "react-router-dom";
 
-import {useNavigate} from "react-router-dom" 
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const [menu, setMenu] = useState("Home");
 
-    const [menu,setMenu] = useState("Home")
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const handleButtonClick = () => {
-      // Navigate to the desired route
-      navigate('/login');
-    };
+  const handleButtonClick = () => {
+    // Navigate to the desired route
+    navigate("/login");
+  };
   return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <p>ADVENTURE DIARY</p>
+      </div>
 
-    <div className='navbar'>
-        <div className='nav-logo'>
-            <p>ADVENTURE DIARY</p>
-               
-        </div>
-        
-        <ul className="nav-menu"> 
+      <ul className="nav-menu">
+        {/* basta mo click kay sa link kay mo redirect ka home, profile, or about */}
+        <li
+          onClick={() => {
+            setMenu("Home");
+          }}
+          n
+        >
+          <Link style={{ textDecoration: "none" }} to="/">
+            Home
+          </Link>
+          {menu === "Home" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("Profile");
+          }}
+          n
+        >
+          <Link style={{ textDecoration: "none" }} to="/profile/:userID">
+            Profile
+          </Link>
+          {menu === "Profile" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("About");
+          }}
+          n
+        >
+          <Link style={{ textDecoration: "none" }} to="/about">
+            About
+          </Link>
+          {menu === "About" ? <hr /> : <></>}
+        </li>
+      </ul>
 
-           {/* basta mo click kay sa link kay mo redirect ka home, profile, or about */}
-           <li onClick={()=>{setMenu("Home")}}n><Link style={{textDecoration:'none'}} to="/">Home</Link>{menu==="Home"?<hr/>:<></>}</li>
-           <li onClick={()=>{setMenu("Profile")}}n><Link style={{textDecoration:'none'}}  to="/profile">Profile</Link>{menu==="Profile"?<hr/>:<></>}</li>
-           <li onClick={()=>{setMenu("About")}}n><Link  style={{textDecoration:'none'}}  to="/about">About</Link>{menu==="About"?<hr/>:<></>}</li>
-             
-        </ul>
+      <div className="log-in">
+        {/* ig click sa button mo render ang login nga page*/}
 
-       <div className="log-in">
-   
-     {/* ig click sa button mo render ang login nga page*/}
-   
-          <button onClick={handleButtonClick}>Login </button>
-       </div>
-
+        <button onClick={handleButtonClick}>Login </button>
+      </div>
     </div>
-  
-  )
-}
+  );
+};
 
 export default NavBar;
